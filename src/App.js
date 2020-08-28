ab31728377b1bfe322e1a77e4fffe4c4865e05aa
Tea Mitchell <teamitchell@rocketmail.com>
2020-08-28 16:25:31 -0500

    updated time clock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+
 import "./App.css";
 import Welcome from "./components/welcome/Welcome";
 import Clock from "./components/clock/Clock";
+import Contact from './components/contacts/Contact';
+import Navigation from './components/navigation/Navigation';
+import Error404 from "./components/error/Error"
+import Jeopardy from "./components/jeopardy/Jeopardy";
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Grande Hey" />
-      <Clock />
+      <Navigation />
+        <Switch>
+          <Route exact
+            path="/"
+            render={(props) => <Welcome {...props} name="Te'a" />} />
+          <Route path="/welcome/:name" render={(props) => <Welcome name={props.match.params.name} />} />
+          <Route path="/clock" component={Clock} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/jeopardy" component={Jeopardy} />
+          <Route component={ Error404 } />
+        </Switch>
     </div>
   );
 }
---
+++ src/components/error/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Error404(props){
+return(
+    <div className="Error"> 
+        <h1>404 <code>no match found</code></h1>
+    </div>
+)
+
+}
+
+export default Error404
